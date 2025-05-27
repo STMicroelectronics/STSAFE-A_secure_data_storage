719ba83b36308364fe285714304b2728be12b67b
Christophe Delaunay <christophe.delaunay@st.com>
2025-05-27 16:10:51 +0200

    Initial release

---
+++ Application/main.c
@@ -0,0 +1,278 @@
+/**
+ ******************************************************************************
+ * \file    		main.c
+ * \author  		CS application team
+ ******************************************************************************
+ *           			COPYRIGHT 2022 STMicroelectronics
+ *
+ * This software is licensed under terms that can be found in the LICENSE file in
+ * the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
+
+/* Includes ------------------------------------------------------------------*/
+#include "Drivers/uart/uart.h"
+#include "stselib.h"
+#include <inttypes.h>
+#include <stdio.h>
+#include <string.h>
+
+/* Defines -------------------------------------------------------------------*/
+#define PRINT_RESET "\x1B[0m"
+#define PRINT_CLEAR_SCREEN "\x1B[1;1H\x1B[2J"
+#define READ_BUFFER_SIZE 64
+#define RANDOM_SIZE 64
+
+/* STDIO redirect */
+#if defined(__GNUC__) && !defined(__ARMCC_VERSION)
+#define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
+#define GETCHAR_PROTOTYPE int __io_getchar()
+#else
+#define PUTCHAR_PROTOTYPE int fputc(int ch, FILE *f)
+#define GETCHAR_PROTOTYPE int fgetc(FILE *f)
+#endif /* __GNUC__ */
+PUTCHAR_PROTOTYPE {
+    uart_putc(ch);
+    return ch;
+}
+GETCHAR_PROTOTYPE {
+    return uart_getc();
+}
+
+void apps_print_data_partition_record_table(stse_Handler_t *pSTSE) {
+    uint8_t i;
+    stse_ReturnCode_t ret;
+    uint16_t data_partition_record_table_length;
+    uint8_t total_zone_count;
+
+    /*- Get total partition count */
+    ret = stse_data_storage_get_total_partition_count(
+        pSTSE,
+        &total_zone_count);
+    if (ret != STSE_OK) {
+        printf("\n\n\r ### stsafe_get_total_partition_count : ERROR 0x%04X", ret);
+        while (1)
+            ;
+    }
+
+    /*- Allocate data partition table  */
+    stsafea_data_partition_record_t data_partition_record_table[total_zone_count];
+
+    /*- Get data partition table  */
+    data_partition_record_table_length = sizeof(data_partition_record_table);
+    ret = stse_data_storage_get_partitioning_table(
+        pSTSE,
+        total_zone_count,
+        data_partition_record_table,
+        data_partition_record_table_length);
+    if (ret != STSE_OK) {
+        printf("\n\r ### stse_get_data_partitions_configuration : ERROR 0x%04X", ret);
+        while (1)
+            ;
+    } else {
+        printf("\n\n\r - stse_get_data_partitions_configuration");
+    }
+
+    printf("\n\r  ID | COUNTER | DATA SEGMENT SIZE | READ AC CR |  READ AC | UPDATE AC CR | UPDATE AC |  COUNTER VAL \r\n");
+    for (i = 0; i < total_zone_count; i++) {
+        /*- print id (col 1)*/
+        printf(" %03d | ", data_partition_record_table[i].index);
+
+        /*- print counter presence (col 2)*/
+        printf("   %c    |", (data_partition_record_table[i].zone_type) == 0 ? '.' : 'x');
+
+        /*- print data segment size (col 3) */
+        printf("       %04u        | ", data_partition_record_table[i].data_segment_length);
+
+        /*- read ac change right (col 4) */
+        printf(" %s | ", (data_partition_record_table[i].read_ac_cr) == 1 ? " ALLOWED " : " DENIED  ");
+
+        /*- read ac right (col 5) */
+        switch (data_partition_record_table[i].read_ac) {
+        case STSE_AC_ALWAYS:
+            printf(" ALWAYS  |");
+            break;
+
+        case STSE_AC_HOST:
+            printf("   HOST  |");
+            break;
+
+        case STSE_AC_AUTH:
+            printf("   AUTH  |");
+            break;
+
+        default:
+            printf("  NEVER  |");
+            break;
+        }
+
+        /*- update ac change right (col 4) */
+        printf(" %s | ", (data_partition_record_table[i].update_ac_cr) == 1 ? "   ALLOWED  " : "   DENIED   ");
+
+        /*- update ac right (col 5) */
+        switch (data_partition_record_table[i].update_ac) {
+        case STSE_AC_ALWAYS:
+            printf("  ALWAYS  |");
+            break;
+
+        case STSE_AC_HOST:
+            printf("   HOST   |");
+            break;
+
+        case STSE_AC_AUTH:
+            printf("  AUTH    |");
+            break;
+
+        default:
+            printf("  NEVER   |");
+            break;
+        }
+
+        printf(" %06" PRIu32 "\r\n", data_partition_record_table[i].counter_value);
+    }
+}
+
+void apps_print_hex_buffer(uint8_t *buffer, uint16_t buffer_size) {
+    uint16_t i;
+    for (i = 0; i < buffer_size; i++) {
+        if (i % 16 == 0) {
+            printf(" \n\r ");
+        }
+        printf(" 0x%02X", buffer[i]);
+    }
+}
+
+void apps_terminal_init(uint32_t baudrate) {
+    (void)baudrate;
+    /* - Initialize UART for example output log (baud 115200)  */
+    uart_init(115200);
+    /* Disable I/O buffering for STDOUT stream*/
+    setvbuf(stdout, NULL, _IONBF, 0);
+    /* - Clear terminal */
+    printf(PRINT_RESET PRINT_CLEAR_SCREEN);
+}
+
+int main(void) {
+    stse_ReturnCode_t stse_ret = STSE_API_INVALID_PARAMETER;
+    stse_Handler_t stse_handler;
+    uint8_t readBuffer[READ_BUFFER_SIZE];
+    uint8_t random[RANDOM_SIZE];
+    uint32_t counter_value;
+
+    /* - Initialize Terminal */
+    apps_terminal_init(115200);
+
+    /* - Print Example instruction on terminal */
+    printf("----------------------------------------------------------------------------------------------------------------");
+    printf("\n\r-                            STSAFE-A120 secure data storage zone access example                               -");
+    printf("\n\r----------------------------------------------------------------------------------------------------------------");
+    printf("\n\r-                                                                                                              -");
+    printf("\n\r- description :                                                                                                -");
+    printf("\n\r- This examples illustrates how to makes use of the STSAFE-A data storage APIs by performing following         -");
+    printf("\n\r- accesses/commands to the target STSAFE device                                                                -");
+    printf("\n\r-          o Query STSAFE-A total partition count                                                              -");
+    printf("\n\r-          o Query STSAFE-A partitions information                                                             -");
+    printf("\n\r-          o Read STSAFE-A zone 5 data & associated counter value                                              -");
+    printf("\n\r-          o Decrement STSAFE-A zone 5                                                                         -");
+    printf("\n\r-          o Read STSAFE-A zone 5 with new data & new associated counter value                                 -");
+    printf("\n\r-                                                                                                              -");
+    printf("\n\r- Note : zone IDs used in this example are aligned with STSAFE-A120 SPL05 personalization                      -");
+    printf("\n\r-        Accesses parameters must be adapted for other device personalization                                  -");
+    printf("\n\r-                                                                                                              -");
+    printf("\n\r----------------------------------------------------------------------------------------------------------------");
+
+    /* ## Initialize STSAFE-A1xx device handler */
+    stse_ret = stse_set_default_handler_value(&stse_handler);
+    if (stse_ret != STSE_OK) {
+        printf("\n\r ## stse_set_default_handler_value ERROR : 0x%04X\n\r", stse_ret);
+        while (1)
+            ;
+    }
+
+    stse_handler.device_type = STSAFE_A120;
+    stse_handler.io.busID = 1; // Needed to use expansion board I2C
+
+    printf("\n\r - Initialize target STSAFE-A120");
+    stse_ret = stse_init(&stse_handler);
+    if (stse_ret != STSE_OK) {
+        printf("\n\r ## stse_init ERROR : 0x%04X\n\r", stse_ret);
+        while (1)
+            ;
+    }
+
+    /* ## Print User NVM data partitioning information */
+    apps_print_data_partition_record_table(&stse_handler);
+
+    /* ## Read Zone 5 */
+    stse_ret = stse_data_storage_read_counter_zone(
+        &stse_handler,      /* SE handler		*/
+        5,                  /* Zone index		*/
+        0x0000,             /* Read Offset		*/
+        readBuffer,         /* Read buffer		*/
+        sizeof(readBuffer), /* Read length		*/
+        04,                 /* Read chunk size	*/
+        &counter_value,     /* Counter Value	*/
+        STSE_NO_PROT);
+    if (stse_ret != STSE_OK) {
+        printf("\n\n\r ### stse_data_storage_read_data_zone : ERROR 0x%04X", stse_ret);
+        while (1)
+            ;
+    } else {
+        printf("\n\n\r - stse_data_storage_read_data_zone (zone : 05 - length : %d - counter : %lu)", sizeof(readBuffer) / sizeof(readBuffer[0]), counter_value);
+        apps_print_hex_buffer(readBuffer, sizeof(readBuffer));
+    }
+
+    /*## Generate random number */
+    stse_ret = stse_generate_random(&stse_handler, random, READ_BUFFER_SIZE);
+    if (stse_ret != STSE_OK) {
+        printf("\n\n\r ### STSAFE-A generate random : ERROR 0x%04X", stse_ret);
+        while (1)
+            ;
+    } else {
+        printf("\n\n\r - stse_generate_random (length : %d)", sizeof(random));
+        apps_print_hex_buffer(random, sizeof(random));
+    }
+
+    /*## Update Zone 5 with Random (Random Number storage)*/
+    stse_ret = stse_data_storage_decrement_counter_zone(
+        &stse_handler,  /* SE handler 			*/
+        5,              /* Zone index 			*/
+        1,              /* Decrement amount		*/
+        0x0000,         /* Update Offset 		*/
+        random,         /* Update input buffer 	*/
+        sizeof(random), /* Update Length 		*/
+        &counter_value, /* Counter value		*/
+        STSE_NO_PROT);
+    if (stse_ret != STSE_OK) {
+        printf("\n\n\r ### stse_data_storage_update_data_zone : ERROR 0x%04X", stse_ret);
+        while (1)
+            ;
+    } else {
+        printf("\n\n\r - stse_data_storage_decrement_counter_zone (zone = 05 - length = %d - New counter : %lu)", sizeof(random) / sizeof(random[0]), counter_value);
+        apps_print_hex_buffer(random, sizeof(random));
+    }
+
+    /* ## Read Zone 5 */
+    stse_ret = stse_data_storage_read_counter_zone(
+        &stse_handler,      /* SE handler		*/
+        5,                  /* Zone index		*/
+        0x0000,             /* Read Offset		*/
+        readBuffer,         /* Read buffer		*/
+        sizeof(readBuffer), /* Read length		*/
+        04,                 /* Read chunk size	*/
+        &counter_value,     /* Counter Value	*/
+        STSE_NO_PROT);
+    if (stse_ret != STSE_OK) {
+        printf("\n\n\r ### stse_data_storage_read_data_zone : ERROR 0x%04X", stse_ret);
+        while (1)
+            ;
+    } else {
+        printf("\n\n\r - stse_data_storage_read_data_zone (zone : 05 - length : %d - New counter : %lu)", sizeof(readBuffer) / sizeof(readBuffer[0]), counter_value);
+        apps_print_hex_buffer(readBuffer, sizeof(readBuffer));
+    }
+
+    while (1)
+        ;
+}
--- README.md
+++ README.md
@@ -1,2 +1,167 @@
-# STSAFE-A_secure_data_storage
-Example illustrating the implementation of STSAFE-A for data storage and device usage monitoring
+
+# STSAFE-A Secure data storage
+This project illustrates how to use the STSAFE-A Secure Element and STMicroelectronics Secure Element Library to perform data storage through STSAFE-A Secure Element.
+When loaded on the target MCU platform , the project performes the storage of 100 bytes through STSAFE-A User-NVM Zone 5. This storage scheme is typically used to store data securely.
+
+```mermaid
+sequenceDiagram
+    box HOST
+    participant HOST as MCU
+    end
+    box Accessory
+    participant STSE as STSAFE-A
+    end
+
+    HOST ->>+ STSE : Get total partition count from User-NVM <br> (stse_data_storage_get_total_partition_count)
+    STSE -->>- HOST : Total partition count
+    HOST ->>+ STSE : Get full data partition table informations from User-NVM <br> (stse_data_storage_get_partitioning_table)
+    STSE -->>- HOST : Data partition records
+    note over HOST : Print data partition records table
+
+    HOST ->>+ STSE : Read User-NVM Zone 1 <br> (100 bytes at offset 0x0000)
+    STSE -->>- HOST : 100 bytes from User-NVM Zone 1
+    note over HOST : Print 100 bytes read from User-NVM Zone 1
+
+    HOST ->>+ STSE : Generate 100 bytes random number <br> (stsafea_generate_random)
+    STSE -->>- HOST : 100 bytes random number
+    HOST ->>+ STSE : Update User-NVM Zone 1 with generated random number <br> (stse_data_storage_update_data_zone) <br> (100 new bytes at offset 0x0000)
+    note over HOST : Print random number (100 bytes)
+    HOST ->>+ STSE : Read User-NVM Zone 1 <br> (100 bytes at offset 0x0000)
+    STSE -->>- HOST : 100 bytes from User-NVM Zone 1
+    note over HOST : Print 100 bytes read from User-NVM Zone 1 <br> These 100 bytes shall be identical <br> to generated 100 bytes random number <br> and different than 100 bytes firstly read
+```
+
+The example applicative flowchart is illustrated below :
+
+```mermaid
+flowchart TD
+    A["MAIN"] --> B["Initialize Apps terminal (baudrate = 115200)"]
+    B --> C["Print example title and instructions"]
+    C --> D["Initialize STSE Handler"]
+    D --> E["Print data partition records table"]
+    E --> F["Print 64 bytes read from User-NVM Zone 5 and associated counter value"]
+    F --> G["Generate Random Number (64 bytes)"]
+    G --> H["Print Random Number"]
+    H --> I["Store Random Number through User-NVM Zone 5"]
+    I --> J["Print 64 bytes read from User-NVM Zone 5 and associated counter value"]
+```
+
+STSELib API & Services used in the example are the following :
+
+- stse_set_default_handler_value
+- stse_init
+- stse_data_storage_get_total_partition_count
+- stse_data_storage_get_partitioning_table
+- stse_data_storage_read_counter_zone
+- stse_generate_random
+- stse_data_storage_decrement_counter_zone
+
+
+## Hardware and Software Prerequisites
+
+- [NUCLEO-L452RE - STM32L452RE evaluation board](https://www.st.com/en/evaluation-tools/nucleo-l452re.html)
+
+- [X-NUCLEO-ESE01A1 - STSAFE-A120 Secure element expansion board](https://www.st.com/en/ecosystems/x-nucleo-ese01a1.html)
+
+- [STM32CubeIDE - Integrated Development Environment for STM32](https://www.st.com/en/development-tools/stm32cubeide.html)
+
+- Serial terminal PC software  (i.e. Teraterm)
+
+> [!NOTE]
+> If you don't use X-NUCLEO-ESE01A1 board, please refer to section how to adapt the exemple
+
+## Getting started with the project
+
+- Connect the [X-NUCLEO-ESE01A1](https://www.st.com/en/ecosystems/x-nucleo-ese01a1.html) expansion board on the top of the [NUCLEO-L452RE](https://www.st.com/en/evaluation-tools/nucleo-l452re.html) evaluation board.
+
+![](./Pictures/X-NUCLEO_eval_kit.png)
+
+- Connect the board to the development computer and Open and configure a terminal software as follow (i.e. Teraterm).
+
+![](./Pictures//teraterm_config.png)
+
+- Open the STM32CubeIDE projects located in Application/STM32CubeIDE
+
+- Build the project by clicking the “**Build the active configurations of selected projects\ **” button and verify that no error is reported by the GCC compiler/Linker.
+
+- Launch a debug session then wait the debugger to stop on the first main routine instruction and press Start button to execute the main routine.
+
+> [!NOTE]
+> - Power configuation Jumper must be set to 3V3-VCC.
+> - The COM port can differ from board to board. Please refer to windows device manager.
+
+<b>Result</b> :
+
+This project reports execution log through the on-board STLINK CDC bridge.
+These logs can be analyzed on development computer using a serial terminal application (i.e.: Teraterm).
+As example below.
+
+<pre>
+----------------------------------------------------------------------------------------------------------------
+-                            STSAFE-A120 secure data storage zone access example                               -
+----------------------------------------------------------------------------------------------------------------
+-                                                                                                              -
+- description :                                                                                                -
+- This examples illustrates how to makes use of the STSAFE-A data storage APIs by performing following         -
+- accesses/commands to the target STSAFE device                                                                -
+-          o Query STSAFE-A total partition count                                                              -
+-          o Query STSAFE-A partitions information                                                             -
+-          o Read STSAFE-A zone 5 data & associated counter value                                              -
+-          o Decrement STSAFE-A zone 5                                                                         -
+-          o Read STSAFE-A zone 5 with new data & new associated counter value                                 -
+-                                                                                                              -
+- Note : zone IDs used in this example are aligned with STSAFE-A120 SPL05 personalization                      -
+-        Accesses parameters must be adapted for other device personalization                                  -
+-                                                                                                              -
+----------------------------------------------------------------------------------------------------------------
+ - Initialize target STSAFE-A120
+
+ - stse_get_data_partitions_configuration
+  ID | COUNTER | DATA SEGMENT SIZE | READ AC CR |  READ AC | UPDATE AC CR | UPDATE AC |  COUNTER VAL
+ 000 |    .    |       1000        |   DENIED   |  ALWAYS  |    DENIED    |   NEVER   | 000000
+ 001 |    .    |       1000        |   ALLOWED  |  ALWAYS  |    ALLOWED   |   ALWAYS  | 000000
+ 002 |    .    |       1000        |   ALLOWED  |  ALWAYS  |    ALLOWED   |   ALWAYS  | 000000
+ 003 |    .    |       1000        |   ALLOWED  |  ALWAYS  |    ALLOWED   |   ALWAYS  | 000000
+ 004 |    .    |       1000        |   ALLOWED  |  ALWAYS  |    ALLOWED   |   ALWAYS  | 000000
+ 005 |    x    |       0064        |   ALLOWED  |  ALWAYS  |    ALLOWED   |   ALWAYS  | 4294967280
+ 006 |    x    |       0064        |   ALLOWED  |  ALWAYS  |    ALLOWED   |   ALWAYS  | 4294967295
+ 007 |    x    |       0064        |   ALLOWED  |  ALWAYS  |    ALLOWED   |   ALWAYS  | 4294967295
+ 008 |    x    |       0064        |   ALLOWED  |  ALWAYS  |    ALLOWED   |   ALWAYS  | 4294967295
+ 009 |    .    |       5120        |   ALLOWED  |  ALWAYS  |    ALLOWED   |   ALWAYS  | 000000
+ 010 |    .    |       3072        |   ALLOWED  |  ALWAYS  |    ALLOWED   |   ALWAYS  | 000000
+ 011 |    .    |       2944        |   ALLOWED  |  ALWAYS  |    ALLOWED   |   ALWAYS  | 000000
+
+
+ - stse_data_storage_read_data_zone (zone : 05 - length : 64 - counter : 4294967280)
+  0xD1 0x78 0x0A 0xB2 0x90 0xE9 0x41 0x74 0xA7 0xC3 0x21 0xE0 0x40 0xC5 0xD3 0xCC
+  0xE5 0x33 0xF6 0xE7 0x2F 0x9C 0x3B 0x46 0x69 0x6E 0x76 0xC5 0x2A 0x28 0x8D 0x11
+  0x7F 0x5F 0x2E 0x80 0xBC 0x63 0x19 0xD0 0xE6 0xA2 0xCD 0xF2 0x75 0x00 0xCD 0xC8
+  0x45 0x1E 0x97 0xB8 0xC2 0xAD 0x66 0x1C 0x42 0xA8 0x68 0x56 0x00 0x55 0xB7 0x1E
+
+ - stse_generate_random (length : 64)
+  0xCD 0x13 0xFA 0x13 0x05 0x5F 0x3E 0x1E 0x54 0x0C 0xB0 0x12 0xEA 0x69 0x0F 0xED
+  0x6F 0x46 0x55 0x0C 0x6B 0x5E 0x80 0xC8 0x41 0xCF 0x74 0x65 0xDC 0x08 0x7C 0xFF
+  0x00 0xA5 0xB8 0xC1 0x48 0x19 0xC6 0x2D 0xA1 0xE2 0x75 0x3A 0x7D 0x3B 0x5B 0x16
+  0x54 0x97 0x76 0x90 0x42 0x71 0xFE 0x9B 0x91 0x2D 0xFF 0x1F 0x98 0x19 0xE6 0x3B
+
+ - stse_data_storage_decrement_counter_zone (zone = 05 - length = 64 - New counter : 4294967279)
+  0xCD 0x13 0xFA 0x13 0x05 0x5F 0x3E 0x1E 0x54 0x0C 0xB0 0x12 0xEA 0x69 0x0F 0xED
+  0x6F 0x46 0x55 0x0C 0x6B 0x5E 0x80 0xC8 0x41 0xCF 0x74 0x65 0xDC 0x08 0x7C 0xFF
+  0x00 0xA5 0xB8 0xC1 0x48 0x19 0xC6 0x2D 0xA1 0xE2 0x75 0x3A 0x7D 0x3B 0x5B 0x16
+  0x54 0x97 0x76 0x90 0x42 0x71 0xFE 0x9B 0x91 0x2D 0xFF 0x1F 0x98 0x19 0xE6 0x3B
+
+ - stse_data_storage_read_data_zone (zone : 05 - length : 64 - New counter : 4294967279)
+  0xCD 0x13 0xFA 0x13 0x05 0x5F 0x3E 0x1E 0x54 0x0C 0xB0 0x12 0xEA 0x69 0x0F 0xED
+  0x6F 0x46 0x55 0x0C 0x6B 0x5E 0x80 0xC8 0x41 0xCF 0x74 0x65 0xDC 0x08 0x7C 0xFF
+  0x00 0xA5 0xB8 0xC1 0x48 0x19 0xC6 0x2D 0xA1 0xE2 0x75 0x3A 0x7D 0x3B 0x5B 0x16
+  0x54 0x97 0x76 0x90 0x42 0x71 0xFE 0x9B 0x91 0x2D 0xFF 0x1F 0x98 0x19 0xE6 0x3B
+</pre>
+
+ ## How to adapt the exemple
+
+ Each STSE handler contains SE's informations mandatory by the STSELib to manage correctly targeted API and services.
+-	device_type : STSAFE-A100/ STSAFE-A110/ STSAFE-A120
+-	pPersoInfo  : pointer to a specific device perso configuration
+-	pActive_host_session : active host session managed by the open session API set
+-	pActive_other_session : other session managed by the open session API set.
+-	Io : communication bus description like I2C device address , I2C speed
